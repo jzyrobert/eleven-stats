86f2277a8da5d7a3003a46a0ff6f3cb1b3403f2e
Robert Jin <jzyrobert@googlemail.com>
2021-05-18 15:33:08 +0100

    Remove old import

--- src/charts/index.ts
+++ src/charts/index.ts
@@ -3,8 +3,3 @@ export {default as MostPlayedChart} from "./MostPlayedChart.vue"
 export {default as MostWonChart} from "./MostWonChart.vue"
 export {default as MostLostChart} from "./MostLostChart.vue"
 // export {default as Chart} from "./Chart.vue"
-
-import { Chart } from "chart.js";
-import { WordCloudController, WordElement } from "chartjs-chart-wordcloud";
-
-Chart.register(WordCloudController, WordElement)
